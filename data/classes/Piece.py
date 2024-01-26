09829c5449b09c40957520047705d675ec7dc4e4
Ilyes Kabouch <ilyes.kabouch@gmail.com>
2024-01-26 15:01:18 -0700

    Add movement rules for pieces and fix bugs in Piece class

--- data/classes/Piece.py
+++ data/classes/Piece.py
@@ -12,8 +12,18 @@ class Piece:
     def is_valid_move(self, current_position, target_position, board):
         dx = target_position[0] - current_position[0]
         dy = target_position[1] - current_position[1]
+        #Movement rules for Pieces. None of the pieces can move through other pieces. None of the pieces can move to a square occupied by a piece of the same color. None of the pieces can move to a square that is not on the board.
+        if target_position[0] < 0 or target_position[0] > 7 or target_position[1] < 0 or target_position[1] > 7:
+            # Can't move off the board
+            return False
+        if board[target_position[0]][target_position[1]].piece is not None and \
+              board[target_position[0]][target_position[1]].piece.color == self.color:
+            # Can't move to a square occupied by a piece of the same color
+            return False
         if self.type == 'Pawn':
+            # Pawns can only move forward one or two squares on their first move. After that, they can only move forward one square. They can also move diagonally one square to capture an opponent's piece. Pawns can never move backwards. Pawns can never move onto a square occupied by a piece of the same color. Pawns can never move through other pieces. Pawns can never move onto a square that is occupied by an opponent's piece unless they are capturing that piece. Pawns can never move diagonally unless they are capturing an opponent's piece.
             if self.color == 'W':
+                # White pawns can only move up the board
                 if dx == 0 and (dy == -1 or (dy == -2 and current_position[1] == 6)):
                     if dy == -1:
                         if board[current_position[0]][current_position[1] - 1].piece is not None:
@@ -42,6 +52,9 @@ class Piece:
                 else:
                     return False
         elif self.type == 'Rook':
+            # Rooks cannot move to its current position
+            if current_position == target_position:
+                return False
             if dx != 0 and dy != 0:
                 # Rooks can only move in straight lines
                 return False
@@ -60,6 +73,8 @@ class Piece:
         elif self.type == 'Knight':
             return abs(dx) == 2 and abs(dy) == 1 or abs(dx) == 1 and abs(dy) == 2
         elif self.type == 'Bishop':
+            if current_position == target_position:
+                return False
             if abs(dx) != abs(dy):
                 return False
             step_x = 1 if dx > 0 else -1
@@ -69,24 +84,31 @@ class Piece:
                 if board[x][y].piece is not None:
                     return False
         elif self.type == 'Queen':
-            if dx == 0 or dy == 0 or abs(dx) == abs(dy):
+            # Queen has the combined movement of Rook and Bishop
+            if current_position == target_position:
+                return False
+            if dx == 0 or dy == 0:
+                # Rook movement
                 if dx != 0:
                     step = 1 if dx > 0 else -1
                     for x in range(current_position[0] + step, target_position[0], step):
                         if board[x][current_position[1]].piece is not None:
+                            # There's a piece in the way
                             return False
-                elif dy != 0:
+                else:
                     step = 1 if dy > 0 else -1
                     for y in range(current_position[1] + step, target_position[1], step):
                         if board[current_position[0]][y].piece is not None:
+                            # There's a piece in the way
                             return False
-                else:
-                    step_x = 1 if dx > 0 else -1
-                    step_y = 1 if dy > 0 else -1
-                    for x, y in zip(range(current_position[0] + step_x, target_position[0], step_x),
-                                     range(current_position[1] + step_y, target_position[1], step_y)):
-                        if board[x][y].piece is not None:
-                            return False
+            elif abs(dx) == abs(dy):
+                # Bishop movement
+                step_x = 1 if dx > 0 else -1
+                step_y = 1 if dy > 0 else -1
+                for x, y in zip(range(current_position[0] + step_x, target_position[0], step_x),
+                                range(current_position[1] + step_y, target_position[1], step_y)):
+                    if board[x][y].piece is not None:
+                        return False
             else:
                 return False
         elif self.type == 'King':
